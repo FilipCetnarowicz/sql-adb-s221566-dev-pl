f502dcf0242d54e4f179ad10146b9a3332d2d3a2
FilipCetnarowicz <fcetnarowicz@gmail.com>
2026-01-04 15:11:06 +0100

    stan po labie 3

---
+++ ZaawansowaneBazyDanych/ZaawansowaneBazyDanych/SalesLT/XMLSchemaCollections/ProductAttributeSchema.sql
@@ -0,0 +1,19 @@
+﻿CREATE XML SCHEMA COLLECTION [SalesLT].[ProductAttributeSchema]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="ProductAttributes">
+    <xsd:complexType>
+      <xsd:complexContent>
+        <xsd:restriction base="xsd:anyType">
+          <xsd:sequence>
+            <xsd:element name="Weight" type="xsd:decimal" />
+            <xsd:element name="Height" type="xsd:decimal" />
+            <xsd:element name="Width" type="xsd:decimal" />
+            <xsd:element name="Length" type="xsd:decimal" />
+            <xsd:element name="Color" type="xsd:string" />
+          </xsd:sequence>
+        </xsd:restriction>
+      </xsd:complexContent>
+    </xsd:complexType>
+  </xsd:element>
+</xsd:schema>';
+
